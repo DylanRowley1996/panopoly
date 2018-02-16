8d038834acf1a42a83b8a81be82475c01e11ea26
DylanRowley1996 <dylan.rowley@ucdconnect.ie>
2018-02-16 15:13:43 +0000

    Slightly refactored Buy and Auction Panel
    Signed-off-by: DylanRowley1996 <dylan.rowley@ucdconnect.ie>

--- src/BuyOrAuction.java
+++ src/BuyOrAuction.java
@@ -10,50 +10,43 @@ public class BuyOrAuction extends JFrame{
 
     private JPanel propertyInformation = new JPanel();
     private JPanel buttonPanel = new JPanel(new GridLayout(1,2));
-    private JSplitPane propertyInformation1 = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+    private JSplitPane propertyCard = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
     private JTextPane propertyName = new JTextPane();
     private JTextArea propertyPrices = new JTextArea("Prices will go here.\n\n\n\n\n\n\n");
     private JButton buy = new JButton("Buy");
     private JButton auction = new JButton("Auction");
-    private String property = "";
 
     //Change parameter to Property property when class is created and implemented
     public BuyOrAuction(String property){
 
+        //Set up header of property card.
         StyledDocument doc = propertyName.getStyledDocument();
         SimpleAttributeSet center = new SimpleAttributeSet();
         StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
         doc.setParagraphAttributes(0, doc.getLength(), center, false);
-
         propertyName.setBackground(Color.decode("#E53C00"));
         propertyName.setText("Property Name");
         propertyName.setEditable(false);
 
+        //Set properties of JTextArea displaying properties price info then add to JPanel.
         propertyPrices.setLineWrap(true);
         propertyPrices.setEditable(false);
-
         propertyInformation.setLayout(new BorderLayout());
         propertyInformation.add(propertyPrices);
 
-        //Implement necessary fields in split pane.
-        propertyInformation1.setTopComponent(propertyName);
-
-        //Add JPanel with property prices to SplitPane
-        propertyInformation1.setBottomComponent(propertyInformation);
-
-        this.property = property;
-
-        //Add Panel and Buttons to JFrame
-        add(propertyInformation1);
+        //Top compononent is name of Property.
+        propertyCard.setTopComponent(propertyName);
+        //Bottom component is the properties pricing.
+        propertyCard.setBottomComponent(propertyInformation);
 
         //Add buttons to button panel
         buttonPanel.add(buy);
         buttonPanel.add(auction);
 
-        //Add button panel to JFrame
+        //Add fully completed property card and buttons to JFrame
+        add(propertyCard);
         add(buttonPanel, BorderLayout.SOUTH);
 
-        //Check if current selection has changed.
         ActionListener buttonListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -69,19 +62,13 @@ public class BuyOrAuction extends JFrame{
 
             }
         };
-
+        
         buy.addActionListener(buttonListener);
         auction.addActionListener(buttonListener);
 
         pack();
         setLocationRelativeTo(null);
-
         setSize(250,300);
-
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
-
-
-
-
 }
