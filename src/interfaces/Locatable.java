ecbe6720653c5ff2b8062132af99fb2bc86115ef
granter1 <sean.grant.1@ucdconnect.ie>
2018-05-02 22:13:02 +0100

    Revert "update MCQ from master"
    This reverts commit dfcf013643a1a75377723e00cb112aab06f50488, reversing
    changes made to f231896d6233dda066cf82b3c1cc46a490701cb6.

--- src/interfaces/Locatable.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public interface Locatable extends Identifiable {
-	
-	public Locatable getLeft();
-	public Locatable getRight();
-	public void setLeft(Locatable l);
-	public void setRight(Locatable r);
-}
