2629121fc56cf0e1d8b6387b5513c84d6cea4b23
DylanRowley1996 <dylan.rowley@ucdconnect.ie>
2018-05-07 12:29:39 +0100

    Sell.java now fully implemented but 1/2 bugs persist. Selected properties are only removed one by one instead of in groups

--- src/interfaces/Improvable.java
+++ src/interfaces/Improvable.java
@@ -4,6 +4,7 @@ public interface Improvable extends Ownable
 {
 
 	public int getNumHouses();
+	public void removeHouse(int number);
 	public int getNumHotels();
 	public void buildHouses(int h);
 	public void buildHotel();
